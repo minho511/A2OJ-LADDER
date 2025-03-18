5ca6d9841ba27d44f21ca9b8001b268c55c55bc7
minho511 <mhkim98@seoultech.ac.kr>
2025-03-18 13:14:37 +0900

    20250318

--- Codeforces Rating < 1300/30_Effective_Approach.cpp
+++ Codeforces Rating < 1300/30_Effective_Approach.cpp
@@ -3,5 +3,24 @@
 using namespace std;
 
 int main(){
-    
+    int n,m;
+    int input;
+    long long cnt1 = 0;
+    long long cnt2 = 0;
+    cin >> n;
+    vector<int> arr(100001);
+    for(int i = 0; i<n; i++){
+        cin >> input;
+        arr[input] = i;
+    }
+    cin >> m;
+    for(int j = 0; j<m; j++){
+        cin >> input;
+        cnt1+=arr[input]+1;
+        cnt2+=n-arr[input];
+        
+    }
+
+    cout << cnt1 << ' ' << cnt2;
+
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://earthshakira.github.io/a2oj-clientside/server/Ladders.html
 
-If anyone has a better alternative solution, please let me know via "Issue" or email.
+If anyone has a better alternative solution, please let me know via "Issues" or email.
